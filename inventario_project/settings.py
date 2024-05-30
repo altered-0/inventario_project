34da6289fa100c905dd50643c7f680fb1931fd5d
altered-0 <xliar06@gmail.com>
2024-05-30 12:42:04 -0300

    added creation of crud with ModelviewSet

--- inventario_project/settings.py
+++ inventario_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "inventario"
 
 ]
 
@@ -77,8 +78,12 @@ WSGI_APPLICATION = "inventario_project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "bd_inventario",
+        "USER": "postgres",
+        "PASSWORD": "140249",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
